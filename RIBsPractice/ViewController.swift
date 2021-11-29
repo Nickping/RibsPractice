401c37b9fa7ebe627bfa955d30f401cf91f78153
Euijoon Jung <jung.euijoon@kt.com>
2021-11-29 17:42:39 +0900

    Initial Commit

---
+++ RIBsPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RIBsPractice
+//
+//  Created by Euijoon Jung on 2021/11/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
