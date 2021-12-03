825b3dc033a77b879f630d04be6229b9c103c8e0
Euijoon Jung <jung.euijoon@kt.com>
2021-12-03 13:42:14 +0900

    second commit

---
+++ RIBsPractice/Main/FavoriteRepoStream.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  RIBsPractice
+//
+//  Created by Euijoon Jung on 2021/12/03.
+//
+
+import Foundation
---
+++ RIBsPractice/views/FavoriteRepoTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  FavoriteRepoTableViewCell.swift
+//  RIBsPractice
+//
+//  Created by Euijoon Jung on 2021/12/03.
+//
+
+import UIKit
+
+class FavoriteRepoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
